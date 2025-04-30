778d9bfffe99d9c70e18254a0f30792c58953df8
Marcos Ribeiro <mpribeiro63@gmail.com>
2025-04-30 00:39:56 -0300

    Update teste_validacao_user.py

--- CADASTRO/teste_validacao_user.py
+++ CADASTRO/teste_validacao_user.py
@@ -1,13 +1,22 @@
-cadastro = [{'Usuario': 'mparcos7'}]
+#CODIGO SÓ EM PYTHON
+#cadastro = [{'Usuario': 'mparcos7'}]
+#def validandoUsuario(cadastro, user):
+#    for pessoas in cadastro:
+#        if pessoas['Usuario'] == user:
+#            print("Usuario ja existe!")
+#            return False
 
-def validandoUsuario(cadastro, user):
-    for pessoas in cadastro:
-        if pessoas['Usuario'] == user:
-            print("Usuario ja existe!")
-            return False
 
+#login = input("Insira o usuario: ")
 
-login = input("Insira o usuario: ")
+#print(validandoUsuario(cadastro, login))
+#print(cadastro)
 
-print(validandoUsuario(cadastro, login))
-print(cadastro)
+# CODIGO CONVERSANDO COM POSTGRESQL
+#def validarUser(nome, id, senha):
+#    cursor.execute("SELECT usuario FROM Usuarios WHERE usuario = '%s';" % (id))
+#    resultado = cursor.fetchall()
+#    if resultado:
+#        return print('Usuario já existente')
+#    else:
+#       return cursor.execute("INSERT INTO Usuarios(nome, usuario, senha) VALUES('%s', '%s', '%s');" % (nome, id, senha))
